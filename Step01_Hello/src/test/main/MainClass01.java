79914b11264c3fb7a315ee2b8683f4e1410a5ff6
Winnie <yeonsu100@gmail.com>
2019-12-31 10:52:50 +0900

    add method

--- Step01_Hello/src/test/main/MainClass01.java
+++ Step01_Hello/src/test/main/MainClass01.java
@@ -7,8 +7,10 @@ public class MainClass01 {
 	public static void main(String[] args) {
 		System.out.println("print test");
 		System.out.println("Winnie Baek");
+		
 		Car car1=new Car();
 		car1.drive();
+		
 		Phone p1=new Phone();
 		p1.call();
 	}
--- Step01_Hello/src/test/mypac/Phone.java
+++ Step01_Hello/src/test/mypac/Phone.java
@@ -4,4 +4,7 @@ public class Phone {
 	public void call() {
 		System.out.println("Ringing~");
 	}
+	public void takeAPicture() {
+		System.out.println("Let's take a Picture");
+	}
 }
