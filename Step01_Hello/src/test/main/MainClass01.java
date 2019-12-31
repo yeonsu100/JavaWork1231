564695a10a50e8e6abe990d40734b943028c13dc
Winnie <yeonsu100@gmail.com>
2019-12-31 10:34:20 +0900

    not Rin but Run

--- Step01_Hello/src/test/main/MainClass01.java
+++ Step01_Hello/src/test/main/MainClass01.java
@@ -1,8 +1,12 @@
 package test.main;
 
+import test.mypac.Car;
+
 public class MainClass01 {
 	public static void main(String[] args) {
 		System.out.println("print test");
 		System.out.println("Winnie Baek");
+		Car car1=new Car();
+		car1.drive();
 	}
 }
